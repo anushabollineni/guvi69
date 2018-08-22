00e046ab4c8b69bd40d7161657dbaec7791f92f4
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-08-22 14:48:28 +0530

    Create guvi69.java

---
+++ guvi69.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+Scanner s=new Scanner(System.in);
+       n=s.nextInt();
+   while(n<=1000)
+{
+if(n%10==0)
+{
+System.out.print(n);
+break;
+}
+n++;
+	}
+}
+}
